05c4b3e5fc46925d96fe77bdb357d34656284c9b
jolinboy <atti.alive@gmail.com>
2014-03-30 10:57:49 +0900

    Create If-else.java

---
+++ If-else.java
@@ -0,0 +1,12 @@
+class IfElse {
+public static void main(String[] args) {
+int a, b, c;
+a=b=c=0;
+for( ; ; ) {
+if(2*a+3*b+4*c==15) {
+System.out.println("A:"+a+""+"B:"+b+"C:"+c);
+break; }
+a++;
+b++;
+c++;
+} } }
